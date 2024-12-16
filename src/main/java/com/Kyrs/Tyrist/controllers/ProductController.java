c4ae664b7c5f1a5ed81c6a67e805e07b2efa3760
Nick <64158017+Nick0070@users.noreply.github.com>
2024-12-16 13:32:51 +0300

    sql see

--- src/main/java/com/Kyrs/Tyrist/controllers/ProductController.java
+++ src/main/java/com/Kyrs/Tyrist/controllers/ProductController.java
@@ -1,14 +1,26 @@
 package com.Kyrs.Tyrist.controllers;
 
+import com.Kyrs.Tyrist.models.Product;
 import com.Kyrs.Tyrist.services.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
 
     @Autowired
-    private ProductsRepository repo;;
+    private ProductsRepository repo;
+
+    @GetMapping({"","/"})
+    public String showProductList (Model model) {
+        List<Product> products = repo.findAll();
+        model.addAttribute ("products",products);
+        return "products/index";
+    }
 }
