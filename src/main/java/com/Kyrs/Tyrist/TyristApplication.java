1c5977b867bea392baf4ccadc112bfbf99e35ed2
Nick <64158017+Nick0070@users.noreply.github.com>
2024-12-16 10:11:12 +0300

    first

---
+++ src/main/java/com/Kyrs/Tyrist/TyristApplication.java
@@ -0,0 +1,16 @@
+package com.Kyrs.Tyrist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyristApplication.class, args);
+	}
+
+}
+
+
+
---
+++ src/test/java/com/Kyrs/Tyrist/TyristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kyrs.Tyrist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
